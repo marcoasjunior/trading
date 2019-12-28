f23e4c5267e336b92c05384df97e6b78d1ecc4aa
Marco <marcoasjunior90@gmail.com>
2019-12-28 15:04:01 -0300

    esqueleto modal de criação de item e usuario

--- src/components/ListItemsAdministration.vue
+++ src/components/ListItemsAdministration.vue
@@ -1,39 +1,91 @@
 <template>
-  <v-card flat>
-          <v-container>
-            <v-row>
+    <div>
+        <v-card flat>
+            <v-container>
+                <v-row>
 
-              <v-col cols="12">
-                <v-list>
-                  <v-list-group v-for="item in items" :key="item.title" v-model="item.active"
-                    :prepend-icon="item.action" no-action>
-                    <template v-slot:activator>
-                      <v-list-item-content>
-                        <v-list-item-title v-text="item.title"></v-list-item-title>
-                      </v-list-item-content>
-                    </template>
+                    <v-col cols="12">
+                        <v-list>
+                            <v-list-group v-for="item in items" :key="item.title" v-model="item.active"
+                                :prepend-icon="item.action" no-action>
+                                <template v-slot:activator>
+                                    <v-list-item-content>
+                                        <v-list-item-title v-text="item.title"></v-list-item-title>
+                                    </v-list-item-content>
+                                </template>
 
-                    <v-list-item v-for="subItem in item.items" :key="subItem.title">
+                                <v-list-item v-for="subItem in item.items" :key="subItem.title">
 
-                      <v-list-item v-text="subItem.title"></v-list-item>
-                      <v-btn>
-                        <v-icon>mdi-plus</v-icon>
-                      </v-btn>
+                                    <v-list-item v-text="subItem.title"></v-list-item>
+                                    <v-btn>
+                                        <v-icon>mdi-plus</v-icon>
+                                    </v-btn>
 
-                    </v-list-item>
-                  </v-list-group>
-                </v-list>
-              </v-col>
-            </v-row>
-          </v-container>
-
+                                </v-list-item>
+                            </v-list-group>
+                        </v-list>
+                        <v-fab-transition>
+                            <v-btn @click="modal = true" color="purple" fab dark absolute bottom right>
+                                <v-icon>mdi-plus</v-icon>
+                            </v-btn>
+                        </v-fab-transition>
+                    </v-col>
+                </v-row>
+            </v-container>
         </v-card>
+
+        <v-dialog v-model="modal">
+            <v-card>
+                <v-toolbar color="deep-purple accent-4" dark>
+                    <v-toolbar-title>Novo Item</v-toolbar-title>
+                    <v-spacer />
+                     <v-btn icon dark @click="modal = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+                </v-toolbar>
+                <v-card-text>
+                    <v-container>
+                        <v-row>
+                            <v-col cols="12" sm="6" md="4">
+                                <v-text-field label="Nome do Item" hint="Descrição do item no campo abaixo" required>
+                                </v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="4">
+                                <v-text-field label="Descrição" required></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="4">
+                                <v-text-field label="Valor de Referência" type="number" step=".01" required>
+                                </v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6">
+                                <v-select
+                                :items="['cat1', 'cat2', 'cat3']"
+                                label="Categoria"
+                                required
+                                ></v-select>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                   
+                    <v-btn color="blue darken-1" text @click="modal = false">Salvar</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+
+
+
+    </div>
+
 </template>
 
 <script>
 export default {
     data () {
       return {
+        modal: false,
         items: [
           {
             action: 'mdi-handshake',
--- src/components/ListUsersAdministration.vue
+++ src/components/ListUsersAdministration.vue
@@ -1,30 +1,83 @@
 <template>
-<v-card flat>
-          <v-container>
-            <v-row>
+    <div>
+        <v-card flat>
+            <v-container>
+                <v-row>
 
-              <v-col cols="12">
-                <v-list>
-                  <v-list-item v-for="item in items" :key="item.title">
-                    <v-list-item-content>
-                      <v-list-item-title v-text="item.title"></v-list-item-title>
-                    </v-list-item-content>
+                    <v-col cols="12">
+                        <v-list>
+                            <v-list-item v-for="item in items" :key="item.title">
+                                <v-list-item-content>
+                                    <v-list-item-title v-text="item.title"></v-list-item-title>
+                                </v-list-item-content>
 
-                    <v-list-item-avatar>
-                      <v-img :src="item.avatar"></v-img>
-                    </v-list-item-avatar>
-                  </v-list-item>
-                </v-list>
-              </v-col>
-            </v-row>
-          </v-container>
+                                <v-list-item-avatar>
+                                    <v-img :src="item.avatar"></v-img>
+                                </v-list-item-avatar>
+                            </v-list-item>
+                        </v-list>
+                        <v-fab-transition>
+                            <v-btn @click="modal = true" color="purple" fab dark absolute bottom right>
+                                <v-icon>mdi-plus</v-icon>
+                            </v-btn>
+                        </v-fab-transition>
+                    </v-col>
+                </v-row>
+            </v-container>
         </v-card>
+
+        <v-dialog v-model="modal">
+            <v-card>
+                <v-toolbar color="deep-purple accent-4" dark>
+                    <v-toolbar-title>Novo Usuário</v-toolbar-title>
+                    <v-spacer />
+                    <v-btn icon dark @click="modal = false">
+                        <v-icon>mdi-close</v-icon>
+                    </v-btn>
+                </v-toolbar>
+                <v-card-text>
+                    <v-container>
+                        <v-row>
+
+                            <v-text-field class="my-n0" v-model="nameContact" label="Nome Completo" required>
+                            </v-text-field>
+
+                            <v-text-field v-model="number" type="text" name="input" hint="Apenas números"
+                                label="Telefone">
+                            </v-text-field>
+
+                            <v-text-field v-model="email" type="email" name="input" label="E-mail">
+                            </v-text-field>
+
+                            <v-text-field v-model="email2" type="email" name="input" label="Confirmar E-mail"
+                                hint="Será o seu LOGIN no sistema">
+                            </v-text-field>
+
+                            <v-text-field v-model="password" type="password" name="input" label="Senha">
+                            </v-text-field>
+
+                            <v-text-field v-model="password2" type="password" name="input" label="Confirmar Senha">
+                            </v-text-field>
+
+                        </v-row>
+                    </v-container>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+
+                    <v-btn color="blue darken-1" text @click="modal = false">Salvar</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+
+    </div>
 </template>
 
 <script>
 export default {
     data () {
       return {
+          modal: false,
         items: [
           {
             action: 'mdi-handshake',
